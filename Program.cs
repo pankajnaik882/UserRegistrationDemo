1496714e47c4a589d23587b074b5567bbe34cbae
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-02 22:20:01 +0530

    Add : Added Program.cs File

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRegistrationDemo
+{
+    class Program
+    {
+        public static void DisplayFName()
+        {
+            Console.WriteLine("Enter your FirstName");
+            string firstName = Console.ReadLine();
+
+            if (Regex.IsMatch(firstName, "^[A-Z][a-z]{2,}$"))
+            {
+                Console.WriteLine("Valid first name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name");
+            }
+
+
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Program.DisplayFName();
+        }
+    }
+}
